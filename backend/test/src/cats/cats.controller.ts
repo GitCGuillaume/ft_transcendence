1e619ca0f1785ac4429bea7f714ffa2d3b67a018
Guillaume CHOPIN <gchopin@e3r5p14.clusters.42paris.fr>
2023-01-02 15:31:13 +0100

    push

--- backend/test/src/cats/cats.controller.ts
+++ backend/test/src/cats/cats.controller.ts
@@ -7,7 +7,7 @@ import {
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 //npm i --save class-validator class-transformer
-import { IsString, IsInt } from 'class-validator';
+import { IsString, IsInt, IsBoolean } from 'class-validator';
 
 //Data transfert Object, define how data will be sent to network, typescript
 class CreateCatDto {
@@ -25,9 +25,10 @@ export class CatsController {
 	//Add a cat to constructor CatsService
 	//curl -d "name=valName&age=10&breed=testBreed" -X POST localhost:8080/cats
 	@Post('cats')
-	async create(@Body() createCatDto: CreateCatDto) {
+	async create(@Body() createCatDto: CreateCatDto): Promise<string> {
 		this.catsService.create(createCatDto);
-		return `post, ${createCatDto.name}\n`;
+		return (`post, name: ${createCatDto.name}\nage: ${createCatDto.age}\nbreed: ${createCatDto.breed}\n`);
+		//return (createCatDto);
 	}
 	@Get('cats')
 	async findAll(): Promise<Cat[]> {
--- frontend/gchopin/my-app/src/index.tsx
+++ frontend/gchopin/my-app/src/index.tsx
@@ -11,6 +11,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
 	<Form />
+  <Form />
+  <Form />
   </React.StrictMode>
 );
 
