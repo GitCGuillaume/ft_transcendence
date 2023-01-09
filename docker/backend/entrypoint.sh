82d89eff0758bc5fad42e1cb3450b28989815f6e
Guillaume CHOPIN <gchopin@e1r8p5.clusters.42paris.fr>
2023-01-09 11:27:56 +0100

    push

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -4,6 +4,8 @@ dockerize -wait tcp://postgres_tr:5432 -timeout 60s
 if [ ! -d ./node_modules ];
 then
 	npm install
+	npm i --save @nestjs/platform-socket.io @nestjs/websockets
+	npm i socket.io
 	npm install --save @nestjs/typeorm typeorm postgresql
 	npm install pg --save
 fi
