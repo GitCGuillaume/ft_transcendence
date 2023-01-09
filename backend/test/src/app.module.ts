cc5c6c513e099fe8725e3a9c5700273f647fdbd5
Guillaume CHOPIN <gchopin@e1r8p5.clusters.42paris.fr>
2023-01-09 09:14:29 +0100

    fuse

--- backend/test/src/app.module.ts
+++ backend/test/src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { ChatGateway } from './chat/chat.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, , TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'postgres_tr',
+        port: 5432,
+        username: 'gchopin',
+        password: 'testpass',
+        database: 'postgres',
+        entities: [],
+        synchronize: false,
+  })],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
 })
